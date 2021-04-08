2fb1d55a9d6969ca96e62fae195e57fb628ca25f
David Boedigheimer <stealth259@gmail.com>
2021-04-08 15:18:52 -0700

    Add to migrations.

--- RecipeBox/Migrations/20210408220352_InstrDetail.Designer.cs
+++ RecipeBox/Migrations/20210408220352_InstrDetail.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecipeBox.Models;
 
 namespace RecipeBox.Migrations
 {
     [DbContext(typeof(RecipeBoxContext))]
-    partial class RecipeBoxContextModelSnapshot : ModelSnapshot
+    [Migration("20210408220352_InstrDetail")]
+    partial class InstrDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RecipeBox/Migrations/20210408220352_InstrDetail.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeBox.Migrations
+{
+    public partial class InstrDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Recipes",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "Tags",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instructions",
+                table: "Recipes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "Instructions",
+                table: "Recipes");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Recipes",
+                newName: "Description");
+        }
+    }
+}
